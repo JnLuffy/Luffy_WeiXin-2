7abdb4cf3a6a3ce454ac4aa3412bd1feb0421922
CooperLuffy <lushihong2012@163.com>
2016-11-04 09:50:08 +0800

    Initial Commit

---
+++ Luffy_WeiXin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Luffy_WeiXin
+//
+//  Created by IOS.Mac on 16/11/4.
+//  Copyright © 2016年 com.elepphant.pingchuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Luffy_WeiXin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Luffy_WeiXin
+//
+//  Created by IOS.Mac on 16/11/4.
+//  Copyright © 2016年 com.elepphant.pingchuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
